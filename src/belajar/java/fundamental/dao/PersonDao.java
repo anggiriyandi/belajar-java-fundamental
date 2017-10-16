32b3e932687d3dfd0a9c78005217fded6679c05e
Anggi Riyandi <anggi.riyandi@gmail.com>
2017-10-16 15:35:55 +0700

    tambah interface

---
+++ src/belajar/java/fundamental/dao/PersonDao.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package belajar.java.fundamental.dao;
+
+import belajar.java.fundamental.Person;
+
+/**
+ *
+ * @author anggi
+ */
+public interface PersonDao {
+    public void simpanPeserson(Person person);
+    public Person getPersonByName(String name);
+    public void updatePerson(String id, Person person);
+}
